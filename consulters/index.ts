d9ef31fd6fe732eb870586a784055021a6d27618
l.murillo.baltodano <l.murillo.baltodano@accenture.com>
2021-09-03 23:28:41 -0600

    add getAssembledMeshesGroup import

--- consulters/index.ts
+++ consulters/index.ts
@@ -1,9 +1,11 @@
+import getAssembledMeshesGroup from './getAssembledMeshesGroup'
 import getAudioProperties from './getAudioProperties'
 import getCanvasRecorder from './getCanvasRecorder'
 import getIsFullscreen from './getIsFullscreen'
 import getCanvasState from './getCanvasState'
 
 export {
+    getAssembledMeshesGroup,
     getAudioProperties,
     getCanvasRecorder,
     getIsFullscreen,
